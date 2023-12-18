411631555e7b4f942f7bc371b9f231ee9e77db34
Yunjiang0 <99641229+Yunjiang0@users.noreply.github.com>
2023-12-18 18:04:09 +0800

    ESLint和prettier的联动配置

--- src/App.vue
+++ src/App.vue
@@ -31,85 +31,73 @@
   </div>
 </template> -->
 <template>
-  <div id="home">
-    <div id="Filter">
-      <div class="all" v-if="screenWidth > 720">
-        <el-row style="height: 15vh;"></el-row>
+    <div id="home">
+        <div id="Filter">
+            <div class="all" v-if="screenWidth > 720">
+                <el-row style="height: 15vh;"></el-row>
+                <el-row :gutter="50">
+                    <el-col :span="12" class="left" :offset="0">
+                        <el-row :gutter="0">
+                            <el-col :xl="20" :lg="22" :md="24" :xm="24" :xs="24" :offset="3">
+                                <helloWorld></helloWorld>
+                            </el-col>
+                        </el-row>
+                        <el-row :gutter="0">
+                            <el-col :span="20" :offset="2">
+                                <yiyan></yiyan>
+                            </el-col>
+                        </el-row>
+                    </el-col>
+                    <el-col :span="12" class="right" :offset="0">
+                        <el-row :gutter="20">
+                            <el-col v-if="screenWidth >= 1000" :span="12" :offset="0">
+                                <wyy></wyy>
+                            </el-col>
+                            <el-col v-if="screenWidth >= 1000" :span="12" :offset="0">
+                                <date></date>
+                            </el-col>
+                            <el-col v-if="screenWidth < 1000" :span="24" :offset="0">
+                                <date></date>
+                            </el-col>
 
-        <el-row :gutter="50">
-          <el-col :span="12" class="left" :offset="0">
-            <el-row :gutter="0">
-              <el-col :xl="20" :lg="22" :md="24" :xm="24" :xs="24" :offset="3">
-                <helloWorld></helloWorld>
-              </el-col>
-            </el-row>
+                        </el-row>
+                        <el-row :gutter="0" style="margin-top: 40px;">
+                            <el-col :span="20" :offset="1">
+                                <linkText></linkText>
+                            </el-col>
+                        </el-row>
+                        <el-row :gutter="0">
+                            <el-col :span="23" :offset="1">
+                                <linkContent></linkContent>
+                            </el-col>
+                        </el-row>
+                    </el-col>
+                </el-row>
+                <el-row style="height: 15vh;"></el-row>
+            </div>
+            <div class="ydAll" v-if="screenWidth <= 720">
+                <el-row style="height: 10vh;"></el-row>
+                <el-row v-show="listStatus == false" class="helloworld">
+                    <helloWorld></helloWorld>
+                </el-row>
+                <el-row v-show="listStatus == false">
+                    <yiyan></yiyan>
+                </el-row>
+                <el-row v-show="listStatus == true">
+                    <date></date>
+                </el-row>
+                <el-row style="margin-top: 20px;" v-show="listStatus == true">
+                    <linkText></linkText>
+                </el-row>
+                <el-row v-show="listStatus == true">
+                    <linkContent></linkContent>
+                </el-row>
+                <menuBtn class="menuBtn" @listChange='getListStatus'></menuBtn>
 
-            <el-row :gutter="0">
-              <el-col :span="20" :offset="2">
-                <yiyan></yiyan>
-              </el-col>
-            </el-row>
-          </el-col>
-          <el-col :span="12" class="right" :offset="0">
-            <el-row :gutter="20">
-              <el-col v-if="screenWidth >= 1000" :span="12" :offset="0">
-                <wyy></wyy>
-              </el-col>
-              <el-col v-if="screenWidth >= 1000" :span="12" :offset="0">
-                <date></date>
-              </el-col>
-              <el-col v-if="screenWidth < 1000" :span="24" :offset="0">
-                <date></date>
-              </el-col>
-
-            </el-row>
-            <el-row :gutter="0" style="margin-top: 40px;">
-              <el-col :span="20" :offset="1">
-                <linkText></linkText>
-              </el-col>
-            </el-row>
-            <el-row :gutter="0">
-              <el-col :span="23" :offset="1">
-                <linkContent></linkContent>
-              </el-col>
-            </el-row>
-          </el-col>
-        </el-row>
-        <el-row style="height: 15vh;"></el-row>
-      </div>
-      <div class="ydAll" v-if="screenWidth <= 720">
-        <el-row style="height: 10vh;"></el-row>
-      <transition name="fade" mode="out-in">
-        <el-row v-show="listStatus == false" class="helloworld">
-          <helloWorld></helloWorld>
-        </el-row>
-      </transition>
-      <transition name="fade"mode="out-in">
-        <el-row v-show="listStatus == false">
-          <yiyan></yiyan>
-        </el-row>
-      </transition>
-      <transition name="fade"mode="out-in">
-        <el-row v-show="listStatus == true">
-          <date></date>
-        </el-row>
-      </transition>
-      <transition name="fade"mode="out-in">
-        <el-row style="margin-top: 20px;" v-show="listStatus == true">
-          <linkText></linkText>
-        </el-row>
-      </transition>
-      <transition name="fade"mode="out-in">
-        <el-row v-show="listStatus == true">
-          <linkContent></linkContent>
-        </el-row>
-      </transition>
-        <menuBtn class="menuBtn" @listChange='getListStatus'></menuBtn>
-
-        <el-row style="height: 15vh;"></el-row>
-      </div>
+                <el-row style="height: 15vh;"></el-row>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 <script setup>
 import { reactive, ref, onMounted, onUnmounted } from 'vue'
@@ -124,10 +112,10 @@ import menuBtn from './components/menu.vue';
 // 移动端按钮状态
 let listStatus = ref(false)
 const getListStatus = () => {
-  console.log(1111);
-  console.log(listStatus.value);
-  listStatus.value = !listStatus.value
-  console.log(listStatus.value);
+    console.log(1111);
+    console.log(listStatus.value);
+    listStatus.value = !listStatus.value
+    console.log(listStatus.value);
 }
 // document.documentElement 是全局变量时
 const el = document.documentElement
@@ -142,79 +130,88 @@ el.style.setProperty('--el-message-bg-color', '#00000040')
 // 获取宽度
 const screenWidth = ref(window.innerWidth)
 const handleResize = () => {
-  screenWidth.value = window.innerWidth
+    screenWidth.value = window.innerWidth
 }
 
 onMounted(() => {
-  window.addEventListener('resize', handleResize)
+    window.addEventListener('resize', handleResize)
 })
 onUnmounted(() => {
-  window.removeEventListener('resize', handleResize)
+    window.removeEventListener('resize', handleResize)
 })
 </script>
 <style scoped>
 .flex-wrap {
-  flex-wrap: 1;
+    flex-wrap: 1;
 }
 
 #home {
-  padding: 0;
-  margin: 0;
-  width: 100%;
-  height: 100%;
-  background: url('https://api.vvhan.com/api/bing?rand=sj') center center no-repeat;
-  background-size: 100vw 100vh;
-  position: absolute;
-  box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    width: 100%;
+    height: 100%;
+    background: url('https://api.vvhan.com/api/bing?rand=sj') center center no-repeat;
+    background-size: 100vw 100vh;
+    position: absolute;
+    box-sizing: border-box;
 }
 
 #Filter {
-  background: radial-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.6));
-  z-index: 1;
-  width: 100%;
-  height: 100%;
-  box-sizing: border-box;
-  padding: 4rem;
+    background: radial-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.6));
+    z-index: 1;
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+    padding: 4rem;
 }
 
 .all {
-  width: 100%;
-  height: 100%;
+    width: 100%;
+    height: 100%;
 }
-.menuBtn{
-  position: absolute;
-  left: 50%;
-  top: 60%;
-  transform: translate(-50%, 0);
-  z-index: 999;
+
+.menuBtn {
+    position: absolute;
+    left: 50%;
+    top: 60%;
+    transform: translate(-50%, 0);
+    z-index: 999;
 }
+
 @media screen and (max-width: 1244px) {
-  .left {
-    padding: 0px !important;
-  }
+    .left {
+        padding: 0px !important;
+    }
 
 
 }
 
 @media screen and (max-width: 930px) {
-  #Filter {
-    padding: 4rem 2rem;
-  }
+    #Filter {
+        padding: 4rem 2rem;
+    }
 }
 
 @media screen and (max-width: 390px) {
-  #Filter {
-    padding: 4rem 1.5rem;
-  }
+    #Filter {
+        padding: 4rem 1.5rem;
+    }
 
-  .helloworld {
-    justify-content: center;
-  }
+    .helloworld {
+        justify-content: center;
+    }
 }
-.fade-enter-active, .fade-leave-active {
-	transition: all 1s
- }
- .fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
-	opacity: 0
-} 
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: all 1s
+}
+
+.fade-enter,
+.fade-leave-to
+
+/* .fade-leave-active in <2.1.8 */
+    {
+    opacity: 0
+}
 </style>
